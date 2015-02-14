e10185226bce3f10108250596fb895029afc74ca
Zalthrion <Development@MacBook-Pro-de-Martha.local>
2015-02-13 21:08:15 -0500

    Sync with master branch

--- src/main/java/com/zalthonethree/zombieinfection/init/EasterEggs.java
+++ src/main/java/com/zalthonethree/zombieinfection/init/EasterEggs.java
@@ -1,15 +1,10 @@
-package com.zalthonethree.zombieinfection;
+package com.zalthonethree.zombieinfection.init;
 
 import com.zalthonethree.zombieinfection.api.ZombieInfectionAPI;
 
-public class EasterEggs {
-	
-	//SPOILERS!!
-	
+public class EasterEggs /*extends EntityDragon*/ {
 	public static void init() {
 		ZombieInfectionAPI.registerEncryptionSwitch("item.mushroomStew", "easteregg.stew", "easteregg.description.stew");
 		ZombieInfectionAPI.registerEncryptionSwitch("item.egg", "easteregg.egg", "easteregg.description.egg");
-		
 	}
-	
 }
